039caa47cc662256e267d64925d37c6f1331ea7f
Murugan P <murugan.palanisamy@yahoo.com>
2020-06-10 17:39:22 +0530

    Added the Dependency Injection

--- MVC5/App_Start/NinjectWebCommon.cs
+++ MVC5/App_Start/NinjectWebCommon.cs
@@ -1,18 +1,16 @@
+using System;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using MVC5;
+using Ninject;
+using Ninject.Web.Common;
 using Ninject.Web.Common.WebHost;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MVC5.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(MVC5.App_Start.NinjectWebCommon), "Stop")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
 
-namespace MVC5.App_Start
+namespace MVC5
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
